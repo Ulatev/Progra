c95d07bc7c9b129a1a36c38c722e28125cc69ef8
Ulatev <127334960+Ulatev@users.noreply.github.com>
2023-06-14 10:32:39 -0600

    Create Pract_2.java

---
+++ Practica/Pract_2.java
@@ -0,0 +1,76 @@
+    package com.mycompany.pract_2;
+
+    /**
+     *
+    * @author Luis Ulate 22/02/2023
+     */
+    public class PRACT_2 {
+    public static void main(String[] args) {
+    
+    //Declaracion de variables y asignacion
+    
+    String nom = "Matias";
+    String ape1 = "Quintana";
+    String ape2 = "Martinez";
+    String NombreCompleto = nom + ape1 + ape2;
+    
+    //Declaracion de asignacion
+      
+    String a = "El ";
+    String b = "mundo ";
+    String c = "esta ";
+    String d = "de cabeza";
+    String Frase = a + b + c + d;
+    
+    //Declaracion de valores numericos
+    
+    int n1, n2, n3, n4;
+    
+    n1 = 120;
+    n2 = 34;
+    n3 = 56;
+    n4 = 5;
+    
+    //Operaciones
+    
+    int suma = n1 + n2 + n3 + n4;
+    int resta = n1 - n2 - n3 - n4;
+    int multiplicacion = n1 * n2 * n3 * n4;
+    double division = n1 / n2 / n3 / n4;
+    double resto = n1 % n2 % n3 % n4;
+    
+    //Impresion de valores
+    
+    System.out.println("Suma: " + suma);
+    System.out.println("Resta: " + resta);
+    System.out.println ("Multiplicacion: " + multiplicacion);
+    System.out.println ("Division: " + division);
+    System.out.println ("Resto: " + resto);    
+     
+        
+    //Incremento de las variables
+    
+    n1++;
+    n4 ++;
+    
+    //Declaracion de la condicion 
+    
+    boolean condicion = true;
+    boolean t = !condicion; 
+    
+    //Declaracion de variables enteras
+    
+    int h = 5;
+    int f = 8;
+    int m = 3;
+    int r = 4;
+    
+    //Operaciones a las variables
+    
+    h += 6;
+    f =-2;
+    m *=2;
+    r /=1;
+      
+    }
+}
