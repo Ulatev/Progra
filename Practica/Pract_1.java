63fd52b3c58b852d99ef9ccab8a9a997acdf8cd8
Ulatev <127334960+Ulatev@users.noreply.github.com>
2023-06-14 10:27:06 -0600

    Create Pract_1.java

---
+++ Practica/Pract_1.java
@@ -0,0 +1,39 @@
+package com.mycompany.pract_1;
+
+/**
+ *
+ * @author Luis Ulate 22/02/2023
+ */
+public class PRACT_1 {
+
+//Ejercicio (b) Derfinicion de las constantes 
+    
+    static final double PI = 3.1416;
+    static final boolean ACTIVO = false;
+    static final double IVA = 0.13;
+    
+    public static void main(String[] args) {
+        
+    //Ejercicio (a) Definicion de las variables   
+        
+    int FDia = 9;
+    String nCedula;
+    double salario;
+    char genero;
+    int edad;
+    boolean esFin = true;
+    String nombre;
+    Boolean esCierto;
+    int Num1 = 2;
+    
+    //Ejercicio (c) Definicion de constantes y variables 
+    
+    salario = 51564;
+    genero = 'F';
+    nCedula = "605780943";
+    edad = 58;
+    esFin = false;
+    Num1 =+1;
+    
+    }
+    }
